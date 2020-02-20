06f0a2900bed0dcb7340dfa790bcbc2a0199c5c3
rakslf <raks@labfellows.com>
2020-02-20 21:01:36 +0530

    Check-in/Check-out script for scanned items with hardcoded location and items

--- barcode_scanner.py
+++ barcode_scanner.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 # Module:        barcode_scanner.py
 # Author:        Raks Raja - Development Team
-# Description:   This module is used to process the barcodes in the S3 bucket.
-# Copyrights:    2020 (c) All Rights Reserved.
+# Description:   This module is used to process the barcodes in the S3 bucket and updates the loaction IDs and the status of item line IDs
+# Copyrights:    2020 (c) All Rights Reserved
 
 # Python libraries
 import requests
 import itertools
 import re
 import logging
+import json
 import boto3
 from boto3.session import Session
 
@@ -40,37 +41,51 @@ class BarcodeScanner:
 
     item_files = [idx for idx in files if idx[0].lower() == 'I'.lower()]
     item_ids = []
+    item_line_ids = []
     for item_file in item_files:
-        item_id = re.findall('\d+', item_file)
-        item_ids.append(item_id)
-    item_ids = list(itertools.chain.from_iterable(item_ids))
+        id = re.findall('\d+', item_file)
+        item_ids.append(id[0])
+        item_line_ids.append(id[1])
+    item_ids = list(dict.fromkeys(item_ids))
 
     print (location_ids)
     print (item_ids)
+    print(item_line_ids)
 
-    # def get_category_id(key):
-    #     url = base_url + 'v2/inventory_items/%s/inventory_item_lines' % (int(item_id[0]))
-    #     location_definition_request = requests.get(url, headers=headers)
-    #     json = location_definition_request.json()
-    #     item_category_ids = []
-    #     for value in json['data']:
-    #         item_category_id = value.get('id', False)
-    #         item_category_ids.append(item_category_id)
-    #         category_url = base_url + 'v2/inventory_item_lines/%s/inventory_audit_lines' % (item_category_id)
-    #         categorty_definition_request = requests.get(category_url, headers=headers)
-    #         category_json = categorty_definition_request.json()
-    #         description = category_json['data'][1].get('description', False)
-    #         check_in_out_url = base_url + 'v2/inventory_item_lines/%s' % (item_category_id)
-    #         if 'checked in' in description or 'Item placed ' in description:
-    #             check_in_out_url_definition_request = requests.patch(check_in_out_url, {"checked_out": False},  headers=headers)
-    #         else:
-    #             check_in_out_url_definition_request = requests.patch(check_in_out_url, {"checked_out": True},headers=headers)
-    #
-    #     return item_category_ids
+    def get_item_line_id(item_line_id, location_id):
+        """Scans the item line and updates the location id and checked in, checked out status """
+        check_in_out_url = Constant.BASE_URL + 'v2/inventory_item_lines/%s' % (item_line_id)
+        category_url = Constant.BASE_URL + 'v2/inventory_item_lines/%s/inventory_audit_lines' % (item_line_id)
+        categorty_definition_request = requests.get(category_url, headers=Constant.headers)
+        response_json = categorty_definition_request.json()
+        description_list = [sub['description'] for sub in response_json['data']]
+        for description in description_list:
+            if 'checked in' in description or 'Item placed ' in description:
+                data = json.dumps({
+                    "checked_out": True,
+                    "checked_in": False,
+                    "location_id":location_id
+                })
+                requests.patch(check_in_out_url, data=data,  headers=Constant.headers) #update the location id and check-in/checkout status
+                continue
+            if 'checked out' in description:
+                data = json.dumps({
+                    "checked_in": True,
+                    "checked_out": False,
+                    "location_id": location_id
+                })
+                requests.patch(check_in_out_url, data=data,headers=Constant.headers) ##update the location id and check-in/checkout status
+                continue
+        logger.info('Location ID and status has been updated successfully for the item line ' + str(item_line_id))
 
-    # category_ids = []
-    # for item_id in item_ids:
-    #     category_id = get_category_id(item_id)
-    #     category_ids.append(category_id)
+    location_id_list = [1, 14, 243, 245, 246]
 
+    item_line_id_list1 = item_line_ids[:5]
+    item_line_id_list2 = item_line_ids[10:15]
+    item_line_id_list2 = item_line_ids[15:20]
+    item_line_id_list2 = item_line_ids[20:25]
+    item_line_id_list2 = item_line_ids[25:30]
 
+    for location_id in location_id_list:
+        for item_line_id in item_line_id_list1:
+            get_item_line_id(item_line_id, location_id)
