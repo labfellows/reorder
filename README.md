cd3ade345cea5f1d7d48aef550e2048d6984f9e6
arvind-labfellows <arvind@labfellows.com>
2020-03-11 09:50:47 -0700

    update script to use new inventory reorder API

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# LabFellows Scripting Examples
+# LabFellows Reordering
 
-This repository provides scripting examples in python for working with our Fusion API. 
+This script demonstrates the automation of min/max based reordering through the FUSION API.
 
-Our Fusion API documentation can be accessed here: https://apidocs.labfellows.com
+Our Fusion API documentation can be accessed here: https://apidocs.labfellows.com.
+
+
+Find the steps below to setup a local python virtual enveronment and run this project:
+
+Step 1: Create a virtual environment: `python3 -m venv venv-reorder`.
+
+Step 2: Activate the virtual environment: `source venv-reorder/bin/activate`.
+
+Step 3: Install requirements: `pip3 install -r requirements.txt`.
+
+Step 4: Run the script: `python3 reordering.py`.
+
+Step 5: Deactivate the virtual environment: `deactivate`.
+
+Step 6: (if required) Remove the virtual environment completely: `rm -r venv-reorder`.
--- reordering.py
+++ reordering.py
@@ -1,113 +1,126 @@
 # *** Reordering Script ***
 
-# This script allows automatic reordering of inventory items when the available count drops below the specified minimum value and reorders to the specified maximum count. 
+# This script allows automatic reordering of inventory items when the available count drops below the specified floor and reorders to the specified ceiling. 
 
 # Set up: 
-# 1. Create a min and max field and set the value on all inventory items they would like automatically reordered. 
-# 2. Specify the minimum and maximum field names within this script.
-# 3. Insert authentication credentials.
-# 4. Set up a cron job for this script or run manually as desired. 
+# 1. Install requests module `pip install requests`
+# 2. Create a min and max field and set the value on all inventory items that need to be automatically reordered. 
+# 3. Specify the min and max field names within this script.
+# 4. Insert authentication credentials.
+# 5. Set up a cron job for this script or run manually as desired. 
 
 # Known script limitations: 
-# 1. Orders with groups that require approvals will still need to be approved in order for the order to go through
-# 2. If items reordered are not yet in inventory when the script runs again, the script will reorder these items again
-# 3. This script auto assigns the first available group to the order 
+# 1. Orders with groups that require approvals will still need to be approved in order for the order to go through.
+# 2. If items reordered are not yet in inventory when the script runs again, the script will reorder these items again.
+# 3. The first available group is auto-assigned to the order.
+# 4. This script does not filter for pending orders. So rerunning before accepting items into inventory could result in duplicates.
 
 import json
 import base64
 import requests
 
-# TODO specify minimum and maximum field names (add these as field definitions to any inventory items you would like reordered)
-minimum = 'Minimum'
-maximum = 'Maximum'
+# TODO specify min and max field names (add these as field definitions to any inventory items you would like reordered)
+MIN_FIELD = 'Minimum'
+MAX_FIELD = 'Maximum'
 
 # TODO insert credentials for authentication (see https://apidocs.labfellows.com for instructions)
-email = "test@test.com"
-api_key = "12345"
-subdomain = "labfellows"
-base_url = "https://api.labfellows.com/"
+BASE_URL = "https://api.labfellows.com/v2/"
+API_KEY = "<YOUR_API_KEY_HERE>"
+email = "<YOUR_EMAIL_HERE>"
+subdomain = "<YOUR_SUBDOMAIN_HERE>"
 
-encoded_auth = base64.encodebytes(('%s:%s' % (email, api_key)).encode('utf8')).decode('utf8').replace('\n', '')
-
+encoded_auth = base64.encodebytes(('%s:%s' % (email, API_KEY)).encode('utf8')).decode('utf8').replace('\n', '')
 headers = {
-    'content-type': "application/vnd.api+json",
-    'accept': "application/vnd.api+json",
-    'x-authorization': "Bearer organization %s" % subdomain,
-    'Authorization': "Basic %s" % encoded_auth
-    }
+  'content-type': "application/vnd.api+json",
+  'accept': "application/vnd.api+json",
+  'x-authorization': "Bearer organization %s" % subdomain,
+  'Authorization': "Basic %s" % encoded_auth
+}
 
-# get field definitions and find id of maximum and minimum
+try:
+  print('get field definitions and id of min and max fields...')
+  inventory_definitions_url = BASE_URL + 'inventory_field_definitions'
+  inventory_definitions_request = requests.get(inventory_definitions_url, headers=headers)
+  inventory_definitions_request.raise_for_status()
+  inventory_definitions = inventory_definitions_request.json()
 
-inventory_definitions_url = base_url + 'v2/inventory_field_definitions'
+  max_field_definition = [el for el in inventory_definitions['data'] if el['name'] == MAX_FIELD]
+  max_field_definition_id = max_field_definition[0]['id'] if len(max_field_definition) > 0 else None
+  if not max_field_definition_id:
+    print('max field needs to be defined for reordering!')
+    exit()
 
-inventory_definitions_request = requests.get(inventory_definitions_url, headers=headers)
-inventory_definitions = inventory_definitions_request.json()
+  min_field_definition = [el for el in inventory_definitions['data'] if el['name'] == MIN_FIELD]
+  min_field_definition_id = min_field_definition[0]['id'] if len(min_field_definition) > 0 else None
 
-max_field_definition = [el for el in inventory_definitions['data'] if el['name'] == maximum]
-min_field_definition = [el for el in inventory_definitions['data'] if el['name'] == minimum]
+  print('get inventory items with the max field name...')
+  inventory_url = BASE_URL + 'inventory_items?inventory_field_definition_id=%s' % max_field_definition_id
+  inventory_request = requests.get(inventory_url, headers=headers)
+  inventory_request.raise_for_status()
+  inventory_items = inventory_request.json()
 
-max_field_definition_id = max_field_definition[0]['id'] if len(max_field_definition) > 0 else None
-min_field_definition_id = min_field_definition[0]['id'] if len(min_field_definition) > 0 else None
+  req_id = None
 
-# get inventory items with the maximum field name 
-inventory_url = base_url + 'v2/inventory_items?inventory_field_definition_id=%s' % max_field_definition_id
+  reorder_items = []
 
-inventory_request = requests.get(inventory_url, headers=headers)
-inventory_items = inventory_request.json()
+  # loop over each inventory item
+  for val in inventory_items['data']:
+    available = val['available']
 
-req_id = None
+    max_obj = [el for el in val['field_values'] if el['definition_id'] == max_field_definition_id]
+    max_val = int(max_obj[0]['value'])  # max value should always be present
 
-# loop over each inventory item
-for val in inventory_items['data']:
-  available = val['available']
+    min_obj = [el for el in val['field_values'] if el['definition_id'] == min_field_definition_id]
+    min_val = int(min_obj[0]['value']) if len(min_obj) > 0 else 0 # set min value to 0 if not set, but will be set to 0 if for some reason not present
 
-  min_obj = [el for el in val['field_values'] if el['definition_id'] == min_field_definition_id]
-  max_obj = [el for el in val['field_values'] if el['definition_id'] == max_field_definition_id]
+    # Reorder items if available count drops below the minVal thresold or if the quantity available is 0)
+    if min_val > available or available == 0:
+      reorder_quantity = max_val - min_val
+      item = val['name']
+      print(f'reordering {item}: {reorder_quantity} units')
+      reorder_items.append({
+        'id': val['id'],
+        'quantity_requested': max_val - min_val
+      })
 
-  min_val = int(min_obj[0]['value']) if len(min_obj) > 0 else 0 # set min value to 0 if not set
-  max_val = int(max_obj[0]['value']) if len(max_obj) > 0 else 0 # max value should always be present, but will be set to 0 if for some reason not present
+  if len(reorder_items) == 0:
+    print('no items to reorder!')
+    exit()
 
-  # Reorder items if available count drops below the minVal thresold or if the quantity available is 0
-  if min_val > available or available == 0:
-    payload = {
-        "lines": [
-          {
-            "catalog_item_id": val['catalog_item']['id'],
-            "quantity_requested": max_val - min_val,
-            "unit_of_measure": val['unit_of_measure'],
-            "price_in_cents": val['price_in_cents']
-          }
-        ]
-    }
+  print('submit items for reorder...')
+  reorder_url = BASE_URL + 'inventory_items/reorder'
+  reorder_request = requests.post(reorder_url, headers=headers, json={'items': reorder_items})
+  reorder_request.raise_for_status()
+  reorder_data = reorder_request.json()
 
-    reorder_url = base_url + 'v2/purchase_order_lines/reorder'
-    reorder_request = requests.post(reorder_url, headers=headers, json=payload)
-    reorder_data = reorder_request.json()
+  req_id = reorder_data['data'][0]['purchase_order']['requisition_id']
+  print(f'requisition_id: {req_id}')
 
-    req_id = reorder_data['data'][0]['purchase_order']['requisition_id']
-
-    print(req_id)
-
-if req_id:
-  # Get available groups 
-  groups_url =  base_url + 'v2/groups/my'
+  print('get available groups...')
+  groups_url =  BASE_URL + 'groups/my'
   groups_request = requests.get(groups_url, headers=headers)
+  groups_request.raise_for_status()
   groups = groups_request.json()
 
-  # assign first group to requisition if user has groups
   if len(groups['data']) > 0:
-    assign_group_url = base_url + 'v2/requisitions/%s' % req_id
-
-    group_payload = {
+    print('assign first group to requisition if user has groups...')
+    assign_group_url = BASE_URL + 'requisitions/%s' % req_id
+    assign_group_request = requests.patch(assign_group_url, headers=headers, json={
       "group_id": groups['data'][0]['id']
-    }
-    assign_group_request = requests.patch(assign_group_url, headers=headers, json=group_payload)
+    })
+    assign_group_request.raise_for_status()
     assign_group_response = assign_group_request.json()
-    print(assign_group_response)
+    # print(assign_group_response)
 
-  # submit requisition
-  submit_url = base_url + 'v2/requisitions/%s/submit' % req_id
-
+  print('submit requisition...')
+  submit_url = BASE_URL + 'requisitions/%s/submit' % req_id
   submit_request = requests.post(submit_url, headers=headers)
+  submit_request.raise_for_status()
   submit_response = submit_request.json()
-  print(submit_response)
+  # print(submit_response)
+
+  print('done!')
+
+except requests.exceptions.RequestException as e:
+  print(e)
+  exit(1)
